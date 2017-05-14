e55c57574dc8cdd41a5e0f4758eded979b0c719e
Joseph Losee <josephlosee@gmail.com>
2017-05-14 11:58:56 -0700

    5/14
    login fail/succ log fully functional, including localization

---
+++ C195_JLosee/Readme.txt
@@ -0,0 +1,4 @@
+C195 Software II
+Decision Making Notes
+-Default User Name/Password: test/test
+-As setting columns to autoincrement being allowed was not noted, when adding a new tuple to the DB, I instead checked the highest ID, the pre-incremented it as the new id for the given tuple.
--- C195_JLosee/src/c195_jlosee/LoginController.java
+++ C195_JLosee/src/c195_jlosee/LoginController.java
@@ -5,10 +5,22 @@
  */
 package c195_jlosee;
 
+
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
+import java.io.Writer;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -22,25 +34,33 @@ import javafx.stage.Stage;
  * @author Joe
  */
 public class LoginController implements Initializable  {
-    @FXML Button loginButton, exitButton;
-    @FXML TextField loginUN;
-    @FXML PasswordField loginPW;
-    @FXML Label  userNameLabel, passwordLabel;
+    @FXML private Button loginButton, exitButton;
+    @FXML private TextField loginUN;
+    @FXML private PasswordField loginPW;
+    @FXML private Label  userNameLabel, passwordLabel;
+    @FXML private ComboBox langSelect;
     Locale userLocale = Locale.getDefault();
     ResourceBundle rb;
+    Path path = Paths.get("./log/login.txt");
+    BufferedWriter logFile;
 
+    ObservableList<String> supportedLangList = FXCollections.observableList(new ArrayList<String>());
     
     @Override public void initialize(URL url, ResourceBundle rb){
         //TODO: Add any init here
-        /*TESTING: userLocale=
 
-         */
-        userLocale = new Locale("de");
-        this.rb = ResourceBundle.getBundle("c195_jlosee/resources/Login", userLocale);
+        supportedLangList.add("English");
+        supportedLangList.add("Francois");
+        supportedLangList.add("Deutch");
 
-        System.out.println("Locale = "+userLocale.toString());
+        langSelect.setItems(supportedLangList);
 
+        changeLanguage(userLocale);
 
+    }
+
+    private void changeLanguage(Locale changeToLocale){
+        this.rb = ResourceBundle.getBundle("c195_jlosee/resources/Login", changeToLocale);
         userNameLabel.setText(this.rb.getString("userName"));
         passwordLabel.setText(this.rb.getString("password"));
         loginButton.setText(this.rb.getString("login"));
@@ -50,25 +70,42 @@ public class LoginController implements Initializable  {
     @FXML public void loginClicked(ActionEvent e){
         //Attempt login, check against SQL database
         //TODO: log failed/successful attempts
-        System.out.println("UN: "+loginUN.getText()+"\nPW: "+loginPW.getText());
-        if (SQLManager.getInstance().login(loginUN.getText(), loginPW.getText())){
-            Alert welcome = new Alert(Alert.AlertType.INFORMATION, rb.getString("loginSuccess"));
-            welcome.showAndWait();
 
-            //ViewManager.showMainView();
-            Stage custStage = new Stage();
-            try {
-                custStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("resources/CustomerView.fxml"))));
-                custStage.show();
-                ViewManager.closeWindowFromEvent(e);
-            } catch (IOException e1) {
-                e1.printStackTrace();
+        //Quick check for blank fields:
+        String userName = loginUN.getText();
+        String pw = loginPW.getText();
+
+        if (userName.isEmpty()){
+            Alert emptyString = new Alert(Alert.AlertType.ERROR, rb.getString("emptyUN"));
+            emptyString.showAndWait();
+        }else if (pw.isEmpty()){
+            Alert emptyString = new Alert(Alert.AlertType.ERROR, rb.getString("emptyPW"));
+            emptyString.showAndWait();
+        } else{
+            if (SQLManager.getInstance().login(userName, pw)) {
+                Alert welcome = new Alert(Alert.AlertType.INFORMATION, rb.getString("loginSuccess"));
+
+                //Write to the log file
+                logAction(true);
+
+                welcome.showAndWait();
+
+                //ViewManager.showMainView();
+                //DEBUG: Show customer view
+                Stage custStage = new Stage();
+                try {
+                    custStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("resources/CustomerView.fxml"))));
+                    custStage.show();
+                    ViewManager.closeWindowFromEvent(e);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+
+            } else {
+                logAction(false);
+                Alert loginFailed = new Alert(Alert.AlertType.ERROR, rb.getString("loginFailed"));
+                loginFailed.showAndWait();//System.out.println(rb.getString("loginFailed"));
             }
-
-        }else{
-            //TODO: Log failed attempt in log file
-            Alert loginFailed = new Alert(Alert.AlertType.ERROR, rb.getString("loginFailed"));
-            loginFailed.showAndWait();//System.out.println(rb.getString("loginFailed"));
         }
     }
     
@@ -78,5 +115,43 @@ public class LoginController implements Initializable  {
         }
 
     }
-    
+
+    @FXML public void langChanged(ActionEvent e){
+        int i = langSelect.getSelectionModel().getSelectedIndex();
+
+        Locale changeTo = new Locale(supportedLangList.get(i).substring(0,2));
+        this.changeLanguage(changeTo);
+    }
+
+    /**
+     * Writes to the log file for login attempts
+     * @param loginSucceeded
+     */
+    private void logAction(boolean loginSucceeded){
+        //Paths.
+        try{
+            Files.createDirectories(Paths.get("./log/"));
+            File test = path.toFile();
+            test.createNewFile();
+        }catch (IOException createExc){
+            System.out.println("Error in creating file: "+createExc.getMessage());
+        }
+
+        String loginString;
+        if (loginSucceeded) {
+            loginString = "" + LocalDateTime.now() + " Login successful for " + loginUN.getText();
+
+        }else{
+            loginString = "" + LocalDateTime.now() + " Login failed for " + loginUN.getText();
+        }
+
+        try (BufferedWriter testBuff = Files.newBufferedWriter(path, Charset.forName("UTF-16"), StandardOpenOption.APPEND)) {
+            testBuff.write(loginString);
+            testBuff.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
